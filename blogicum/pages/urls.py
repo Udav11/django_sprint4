2d95a6b7aa8f3bacf898e44407d1057168516950
Udav <udavimpinf@gmail.com>
2025-02-11 15:58:42 +0300

    asd

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'pages'
 
 urlpatterns = [
-    path('about/', views.AboutView.as_view(), name='about'),
-    path('rules/', views.RulesView.as_view(), name='rules'),
+    path('about/', views.AboutView.as_view(template_name='pages/about.html'), name='about'),
+    path('rules/', views.RulesView.as_view(template_name='pages/rules.html'), name='rules'),
 ]
